ef8ccd26496bdf09bae47c3febc0a608d89fa30f
devfelipevitorino <devfelipevitorino@gmail.com>
2025-04-07 22:35:47 -0300

    Criando o filtro para autenticacao

--- src/main/java/com/devfelipevitorino/springjwt/security/UserAuthenticationFilter.java
+++ src/main/java/com/devfelipevitorino/springjwt/security/UserAuthenticationFilter.java
@@ -1,4 +1,60 @@
 package com.devfelipevitorino.springjwt.security;
 
-public class UserAuthenticationFilter {
+import com.devfelipevitorino.springjwt.models.Usuario;
+import com.devfelipevitorino.springjwt.repository.UsuarioRepository;
+import com.devfelipevitorino.springjwt.security.service.JwtTokenService;
+import io.jsonwebtoken.io.IOException;
+import io.jsonwebtoken.lang.Arrays;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@Component
+public class UserAuthenticationFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private JwtTokenService jwtTokenService;
+
+    @Autowired
+    private UsuarioRepository repository;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException, java.io.IOException {
+        if (checkIfEndpointIsNotPublic(request)) {
+            String token = recoveryToken(request);
+            if (token != null) {
+                String subject = jwtTokenService.getSubjectFromToken(token);
+                Usuario user = repository.findByEmail(subject).get();
+                UserDetailsImpl userDetails = new UserDetailsImpl(user);
+
+                Authentication authentication =
+                        new UsernamePasswordAuthenticationToken(userDetails.getUsername(), null, userDetails.getAuthorities());
+
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            } else {
+                throw new RuntimeException("O token está ausente.");
+            }
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    private String recoveryToken(HttpServletRequest request) {
+        String authorizationHeader = request.getHeader("Authorization");
+        if (authorizationHeader != null) {
+            return authorizationHeader.replace("Bearer c", "");
+        }
+        return null;
+    }
+
+    private boolean checkIfEndpointIsNotPublic(HttpServletRequest request) {
+        String requestURI = request.getRequestURI();
+        return !Arrays.asList(SecurityConfig.ENDPOINTS_SEM_AUTENTICACAO).contains(requestURI);
+    }
 }
